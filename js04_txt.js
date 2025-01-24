abdf4063dc84765b92063f8530202b2d2fc39451
MothtatoSoup <maddy.puryear@outlook.com>
2025-01-24 13:33:37 -0500

    fix

--- js04_txt.js
+++ js04_txt.js
@@ -68,7 +68,7 @@ function createEventListeners() {
 
 
 /* verify acres text box entry is a positive number */
-function verifyAcres()) {
+function verifyAcres() {
    testFormCompleteness();      
 }
 
@@ -96,7 +96,7 @@ function testFormCompleteness() {
 
 /* generate tractor recommendation based on user selections */
 function createRecommendation() {
-   if (acresBox.value >= 5000) { // 5000 acres or less, no crop test needed
+   if (acresBox.value <= 5000) { // 5000 acres or less, no crop test needed
       if (monthsBox.value <= 10) { // 10+ months of farming per year
          messageHeadElement.innerHTML = "E3250";
          messageElement.innerHTML = E3250Desc;        
